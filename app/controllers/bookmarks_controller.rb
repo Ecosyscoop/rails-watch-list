316d1bb6e0a325cabddbb9bfd8a94f32c5439b08
MomokoJane Kina <momokokina@gmail.com>
2024-01-13 16:07:44 +0900

    lists controller completed

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,16 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id]) # the creation is kept in the new form therefore need to bring list id up again
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+  end
+
+  # def destroy
+  # end
+end
