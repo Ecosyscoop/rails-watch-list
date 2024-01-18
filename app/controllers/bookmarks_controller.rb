a8189a97731d3e950963d7d1dd451941ffa22a75
MomokoJane Kina <momokokina@gmail.com>
2024-01-18 19:54:26 +0900

    created Bookmark and showing in html

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,10 +9,15 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     @bookmark.save
+    redirect_to list_path(@list)
   end
 
-  # def destroy
-  # end
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  # No need for app/views/restaurants/destroy.html.erb
+      redirect_to lists_path, status: :see_other
+  end
 
   def bookmark_params
     params.require(:bookmark).permit(:movie_id, :comment)
